f9c7956bf6da76018c07bf67d8e279908bd57402
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-26 11:17:05 +0200

    Adds another pod

--- ChatApp/Login/LoginViewController.swift
+++ ChatApp/Login/LoginViewController.swift
@@ -2,8 +2,17 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+  lazy var loginBottomContainer: LoginBottomContainer = {
+    let view = LoginBottomContainer()
+    return view
+    }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = ColorList.Login.background
+  }
+
+  func setupConstraints() {
+
   }
 }
