f84d5827fbd78a9725a662578f368c56400546aa
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-26 19:24:37 +0200

    Adds another textField.

--- ChatApp/Signup/Views/SignupScreenTextField.swift
+++ ChatApp/Signup/Views/SignupScreenTextField.swift
@@ -1,5 +1,50 @@
 import UIKit
+import Cartography
 
-class SignupScreenTextField: UITextField {
+class SignupScreenTextField: UIView {
 
+  enum Kind {
+    case Name, Email, Password
+  }
+
+  lazy var textField: UITextField = {
+    let textField = UITextField()
+    return textField
+    }()
+
+  lazy var logo: UIImageView = {
+    let imageView = UIImageView()
+    return imageView
+    }()
+
+  var kind: Kind
+
+  init(kind: Kind) {
+    self.kind = kind
+    super.init(frame: CGRectZero)
+
+    setupConfiguration()
+    setupConstraints()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Configuration
+
+  func setupConfiguration() {
+    switch kind {
+    case .Name:
+      print("Name")
+    case .Email:
+      print("Email")
+    case .Password:
+      print("Password")
+    }
+  }
+
+  func setupConstraints() {
+    
+  }
 }
