bbcefa91d3c15a044bd45504cc97be986cd6f415
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-26 10:55:51 +0200

    Adds the parse configuration already

--- ChatApp/AppDelegate/AppDelegate.swift
+++ ChatApp/AppDelegate/AppDelegate.swift
@@ -7,20 +7,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   lazy var chatsViewController: ChatsTableViewController = ChatsTableViewController()
   var window: UIWindow?
 
-  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    window = UIWindow()
-    window?.rootViewController = chatsViewController
-    window?.makeKeyAndVisible()
+  func application(application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+      window = UIWindow()
+      window?.rootViewController = chatsViewController
+      window?.makeKeyAndVisible()
 
-    setupParseConfiguration()
+      setupParseConfiguration()
 
-    return true
+      return true
   }
 
   func setupParseConfiguration() {
     Parse.enableLocalDatastore()
-    Parse.setApplicationId("sP8l2ueODwtmMZ0PPunp4a1urPJ72ZMaIQ5Cy560",
-      clientKey: "Ku6j9PilD0ieGu9g89ZkC8ozyZbTx3YCMuT5CmJc")
+    Parse.setApplicationId(Information.parse.applicationID,
+      clientKey: Information.parse.clientKey)
   }
 }
 
