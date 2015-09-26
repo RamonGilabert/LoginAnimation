c46be95687c6a19af866bd2dcbf9c0fe500b44b1
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-26 12:06:58 +0200

    Adds the main configuration

--- ChatApp/Login/LoginViewController.swift
+++ ChatApp/Login/LoginViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Cartography
 
 class LoginViewController: UIViewController {
 
--- ChatApp/Login/Views/LoginBottomContainer.swift
+++ ChatApp/Login/Views/LoginBottomContainer.swift
@@ -1,5 +1,41 @@
 import UIKit
+import Cartography
 
 class LoginBottomContainer: UIView {
 
+  struct Dimensions {
+    static let buttonHeight: CGFloat = 74
+    static let buttonWidth: CGFloat = 120
+    static let offsetButtons: CGFloat = 5
+    static let offsetBottom: CGFloat = 25
+  }
+
+  lazy var signupButton: UIButton = {
+    let button = UIButton()
+    return button
+    }()
+
+  lazy var loginButton: UIButton = {
+    let button = UIButton()
+    return button
+    }()
+
+  required init() {
+    super.init(frame: CGRectZero)
+
+    for subview in [signupButton, loginButton] { addSubview(subview) }
+
+    setupConstraints()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Constraints
+
+  func setupConstraints() {
+    constrain(signupButton, loginButton) { signup, login in
+    }
+  }
 }
