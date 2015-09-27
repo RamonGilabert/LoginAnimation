ddf2310a87431ad4732436b9b07aa0d8932ceba5
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-27 09:31:17 +0200

    Doing the transition already.

--- ChatApp/Login/LoginTransitionManager.swift
+++ ChatApp/Login/LoginTransitionManager.swift
@@ -4,21 +4,42 @@ class LoginTransitionManager: NSObject {
 
   private var presenting = true
 
+  func presentAnimation(mainController: LoginMainViewController, loginController: LoginViewController, show: Bool) {
+    if show {
+      loginController.view.alpha = 1
+      loginController.view.transform = CGAffineTransformIdentity
+    } else {
+      loginController.view.alpha = 0
+      loginController.view.transform = CGAffineTransformMakeScale(3, 3)
+    }
+  }
+
   func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
-    let container = transitionContext.containerView()
-    let fromView = transitionContext.viewForKey(UITransitionContextFromViewKey)!
-    let toView = transitionContext.viewForKey(UITransitionContextToViewKey)!
+    guard let container = transitionContext.containerView() else { return }
 
-    toView.transform = self.presenting ? CGAffineTransformMakeScale(2, 2) : CGAffineTransformIdentity
+    let duration = transitionDuration(transitionContext)
+    let screens : (from: UIViewController, to: UIViewController) =
+    (transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!,
+      transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!)
 
-    container!.addSubview(toView)
-    container!.addSubview(fromView)
+    let mainViewController = !presenting ? screens.to as! LoginMainViewController
+      : screens.from as! LoginMainViewController
+    let loginViewController = !presenting ? screens.from as! LoginViewController
+      : screens.to as! LoginViewController
 
-    UIView.animateWithDuration(0.5, animations: {
-      fromView.transform = self.presenting ? CGAffineTransformMakeScale(2, 2) : CGAffineTransformIdentity
-      toView.transform = CGAffineTransformIdentity
-      }, completion: { finished in
+    let mainView = mainViewController.view
+    let loginView = loginViewController.view
+
+    if presenting { presentAnimation(mainViewController, loginController: loginViewController, show: false) }
+
+    container.addSubview(mainView)
+    container.addSubview(loginView)
+
+    UIView.animateWithDuration(duration, animations: {
+      self.presentAnimation(mainViewController, loginController: loginViewController, show: self.presenting)
+      }, completion: { _ in
         transitionContext.completeTransition(true)
+        UIApplication.sharedApplication().keyWindow!.addSubview(screens.to.view)
     })
   }
 }
